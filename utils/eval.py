b0a74c0500fc094601faaece5f085173d46bb1e8
sandwich <ge93pen@mytum.de>
2020-11-30 18:46:57 +0100

    MixMatch: modified eval.py file by changing view() to reshape()

--- utils/eval.py
+++ utils/eval.py
@@ -2,6 +2,7 @@ from __future__ import print_function, absolute_import
 
 __all__ = ['accuracy']
 
+
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
@@ -13,6 +14,6 @@ def accuracy(output, target, topk=(1,)):
 
     res = []
     for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
+        correct_k = correct[:k].reshape(-1, 1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
